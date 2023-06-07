f0ac8a7fd5b33e794691233acd989c0988189240
Philipe Pompeu <philipepompeu2@gmail.com>
2023-06-06 23:59:26 -0300

    Criação dos componentes solicitados

--- meus_contratos/src/app/app-routing.module.ts
+++ meus_contratos/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaContratosComponent } from './lista-contratos/lista-contratos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ListaContratosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
